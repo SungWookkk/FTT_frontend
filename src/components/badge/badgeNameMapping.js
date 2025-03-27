00ee4402cf5ee65e8debe74a12d60309ffcd1f57
SungWookkk <patksy68@gmail.com>
2025-03-27 09:56:05 +0900

    3/27 refactor: 사용자 뱃지 BE 매핑 문구 변경
    - BE DB에 들어가는 이름과 매핑

--- src/components/badge/badgeNameMapping.js
+++ src/components/badge/badgeNameMapping.js
@@ -1,4 +1,3 @@
-// badgeMapping.js (예시 파일)
 import badge01 from "../../Auth/css/img/badge_design/Badge_01.svg";
 import badge02 from "../../Auth/css/img/badge_design/Badge_02.svg";
 import badge03 from "../../Auth/css/img/badge_design/Badge_03.svg";
@@ -7,30 +6,41 @@ import badge05 from "../../Auth/css/img/badge_design/Badge_05.svg";
 import badge06 from "../../Auth/css/img/badge_design/Badge_06.svg";
 import badge07 from "../../Auth/css/img/badge_design/Badge_07.svg";
 import badge08 from "../../Auth/css/img/badge_design/Badge_08.svg";
-import badge09 from "../../Auth/css/img/badge_design/Badge_09.svg";
 
-// (1) "Badge_01" → 실제 SVG 파일
+/**
+ * 백엔드 DB의 badge_name 필드 예:
+ * - "뚜벅뚜벅 초심자"
+ * - "목표를 위한 노력가!"
+ * - "꾸준한 실천러!"
+ * - "열일 챔피언!"
+ * - "미루기를 모르는 사람!"
+ * - "갓생을 위한 노력"
+ * - "이정도면 미친 사람"
+ * - "갓생"
+ *
+ * 이를 그대로 키로 사용해서 프론트에서 매핑.
+ */
+
+// (1) "뚜벅뚜벅 초심자" → 실제 SVG 파일
 export const badgeImages = {
-    "Badge_01": badge01,
-    "Badge_02": badge02,
-    "Badge_03": badge03,
-    "Badge_04": badge04,
-    "Badge_05": badge05,
-    "Badge_06": badge06,
-    "Badge_07": badge07,
-    "Badge_08": badge08,
-    "Badge_09": badge09,
+    "뚜벅뚜벅 초심자": badge01,
+    "목표를 위한 노력가!": badge02,
+    "꾸준한 실천러!": badge03,
+    "열일 챔피언!": badge04,
+    "미루기를 모르는 사람!": badge05,
+    "갓생을 위한 노력": badge06,
+    "이정도면 미친 사람": badge07,
+    "갓생": badge08,
 };
 
-// (2) "Badge_01" → 화면에 표시할 문구
+// (2) "뚜벅뚜벅 초심자" → 화면에 표시할 문구
 export const badgeNameMapping = {
-    "Badge_01": "뚜벅뚜벅 초심자",
-    "Badge_02": "목표를 위한 노력",
-    "Badge_03": "꾸준한 실천자",
-    "Badge_04": "열일 챔피언",
-    "Badge_05": "미루기를 모르는 자",
-    "Badge_06": "간성을 위한 노력",
-    "Badge_07": "예시7",
-    "Badge_08": "예시8",
-    "Badge_09": "예시9",
+    "뚜벅뚜벅 초심자": "뚜벅뚜벅 초심자",
+    "목표를 위한 노력가!": "목표를 위한 노력가!",
+    "꾸준한 실천러!": "꾸준한 실천러!",
+    "열일 챔피언!": "열일 챔피언!",
+    "미루기를 모르는 사람!": "미루기를 모르는 사람!",
+    "갓생을 위한 노력": "갓생을 위한 노력",
+    "이정도면 미친 사람": "이정도면 미친 사람",
+    "갓생": "갓생",
 };
