06fc75bc81480d186fa803203ab5c448a0099c3a
SungWookkk <patksy68@gmail.com>
2025-04-05 20:43:28 +0900

    4/5 feat: [팀] 팀 Todo MainPage 헤더 추가
    - 디자인 통일을 위한 헤더 추가

--- src/components/team/TeamTodoContentPage.js
+++ src/components/team/TeamTodoContentPage.js
@@ -1,7 +1,74 @@
+import TeamDropdown from "./TeamDropdown";
+import React from "react";
+import {useHistory, useLocation, useParams} from "react-router-dom";
+import "../team/css/TeamTodoContentPage.css";
+
 const TeamTodoContentPage = () => {
-    return(
-        <h2>2</h2>
-    )
-};
+    const { teamId } = useParams();
+    const history = useHistory();
+    const location = useLocation();   // ← 현재 경로 확인용
+
+    // 현재 경로가 "/team/:teamId"인지 "/team/:teamId/todo"인지 체크
+    const isMainPage = location.pathname === `/team/${teamId}`;
+    const isTodoPage = location.pathname === `/team/${teamId}/todo`;
+
+    return (
+        <div className="dashboard-content">
+            {/* 상단 헤더 */}
+            <div className="dashboard-header">
+                <div className="dashboard-left">
+                    <span className="title-text">팀 공간</span>
+                    <TeamDropdown/>
+                </div>
+            </div>
+
+            {/* 목록 선택 탭 */}
+            <div className="list-tap">
+                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", width: "100%" }}>
+                    <div className="list-tab-container">
+                        {/* 메인 탭 */}
+                        <div
+                            className={`tab-item ${isMainPage ? "active" : ""}`}
+                            onClick={() => history.push(`/team/${teamId}`)}
+                        >
+                            메인
+                        </div>
+                        {/* 팀 Todo 탭 */}
+                        <div
+                            className={`tab-item ${isTodoPage ? "active" : ""}`}
+                            onClick={() => history.push(`/team/${teamId}/todo`)}
+                        >
+                            팀 Todo
+                        </div>
+                        {/* 소통 탭 (예: /team/:teamId/chat) - 경로 준비 시 추가 */}
+                        <div className="tab-item">소통</div>
+                    </div>
+                </div>
+            </div>
+
+            {/* 알림 배너 */}
+            <div className="alert-banner">
+                <p className="alert-text">
+                    <span className="highlight-text">팀</span>
+                    <span className="normal-text">은 공동의 목표를 위해 함께 </span>
+                    <span className="highlight-text">소통하고 협업</span>
+                    <span className="normal-text">
+            {" "}
+                        하는 공간입니다. 서로의 아이디어와 역량을 모아{" "}
+          </span>
+                    <span className="highlight-text">시너지를 발휘</span>
+                    <span className="normal-text">하며, 매일의 과제를 </span>
+                    <span className="highlight-text">함께 해결</span>
+                    <span className="normal-text">
+            {" "}
+                        해 보세요. 작지만 꾸준한 노력들이 모여{" "}
+          </span>
+                    <span className="highlight-text">팀의 성장</span>
+                    <span className="normal-text">을 이끌어냅니다!</span>
+                </p>
+            </div>
+        </div>
+    );
+}
 
 export default TeamTodoContentPage;
