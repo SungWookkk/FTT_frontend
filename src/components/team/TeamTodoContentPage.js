b3cd41187605b5c44c49428e3eb1e79e3c23b764
SungWookkk <patksy68@gmail.com>
2025-04-15 11:24:37 +0900

    4/15 fix: [팀] ContentPage 상단바 경로 추가
    - 소통 버튼 동작 안 하던거 알고보니 경로 추가 안 했어서 추가...

--- src/components/team/TeamTodoContentPage.js
+++ src/components/team/TeamTodoContentPage.js
@@ -204,7 +204,12 @@ function TeamTodoContentPage() {
                         >
                             팀 Todo
                         </div>
-                        <div className="tab-item">소통</div>
+                        <div
+                            className={`tab-item ${isTodoPage ? "active" : ""}`}
+                            onClick={() => history.push(`/team/${teamId}/community`)}
+                        >
+                            소통
+                        </div>
                     </div>
                 </div>
             </div>
