0fa3e9f9f52fc5b637b64b2352a10bba79d8e0bf
SungWookkk <patksy68@gmail.com>
2025-04-16 10:47:22 +0900

    4/16 refactor: [팀] 커뮤니티 페이지 Routing 구조 개선
    - App.js에서의 커뮤니티 라우팅 단독으로 수정
    - TeamCommunityPage에서 중첩 라우팅으로 채널 ID 있는 경로에서 채팅 사이드바 유지

--- src/components/team/TeamCommunityPage.js
+++ src/components/team/TeamCommunityPage.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import Header from "../dashboard/Header";
 import Sidebar from "../dashboard/Sidebar";
 import ChatSidebar from "./ChatSidebar";
 import TeamCommunityContentPage from "./TeamCommunityContentPage";
+import ChannelDetailContentPage from "./ChannelDetailContentPage";
 
 function TeamCommunityPage() {
-    // false: 기본 사이드바, true: 채팅 사이드바
+    const { path } = useRouteMatch();
+    // false: 기본 사이드바, true: 채팅 사이드바 (두 사이드바 전환을 위한 상태)
     const [useChatSidebar, setUseChatSidebar] = useState(true);
 
-    // Sidebar에서 "채팅" 버튼 클릭 시 호출할 콜백
-    const handleShowChat = () => {
-        setUseChatSidebar(true);
-    };
-
-    // ChatSidebar에서 "기본" 버튼 클릭 시 호출할 콜백
-    const handleShowDefault = () => {
-        setUseChatSidebar(false);
-    };
+    // 채팅 사이드바와 기본 사이드바 전환 함수
+    const handleShowChat = () => setUseChatSidebar(true);
+    const handleShowDefault = () => setUseChatSidebar(false);
 
     return (
         <div className="dashboard-wrapper">
             <Header />
-
             {useChatSidebar ? (
                 <ChatSidebar onBack={handleShowDefault} />
             ) : (
                 <Sidebar onToggle={handleShowChat} />
             )}
 
-            <TeamCommunityContentPage />
+            {/* 중첩 라우트 영역 */}
+            <Switch>
+                {/* 채널 ID가 없는 경우 기본 커뮤니티 화면 */}
+                <Route exact path={path} component={TeamCommunityContentPage} />
+                {/* 채널 ID가 있는 경우 채널 상세 화면 */}
+                <Route path={`${path}/:channelId`} component={ChannelDetailContentPage} />
+            </Switch>
         </div>
     );
 }
